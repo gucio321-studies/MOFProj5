996bba1d221a88a08536a33225eade662d9d90d3
gucio321 <gucio321@protonmail.com>
2026-05-14 15:27:03 +0200

    pkg: fix rho issue
    feat @deepseek

--- go/pkg/main.go
+++ go/pkg/main.go
@@ -34,7 +34,7 @@ func (p *Poisson) U(i, j int) float64 {
 }
 
 func (p *Poisson) Rho(i, j int) float64 {
-	x, y := float64(i), float64(j)
+	x, y := float64(i)-N, float64(j)-N
 	d := p.d
 	x0 := p.x0
 	return math.Exp(-(math.Pow((x-x0), 2)+y*y)/(d*d)) - math.Exp(-(math.Pow((x+x0), 2)+y*y)/(d*d))
@@ -44,11 +44,11 @@ func (p *Poisson) OptimizeAt(i, j int) {
 	var s [4]sdelta
 	for k, delta := range p.optimizationDeltas {
 		s[k].delta = delta
-		s[k].s = p.S(i, j, delta)
+		s[k].s = p.SLocal(i, j, delta)
 	}
 
 	s[3].delta = .25 * (3*s[0].s - 4*s[1].s + s[2].s) / (s[0].s - 2*s[1].s + s[2].s)
-	s[3].s = p.S(i, j, s[3].delta)
+	s[3].s = p.SLocal(i, j, s[3].delta)
 
 	sort.Slice(s[:], func(i, j int) bool {
 		return s[i].s < s[j].s
@@ -57,15 +57,18 @@ func (p *Poisson) OptimizeAt(i, j int) {
 	p.u[i][j] += s[0].delta
 }
 
-func (p *Poisson) Optimize() {
+// Optimize does one iteration (optimizeAt for each point). It returns total S value.
+func (p *Poisson) Optimize() float64 {
 	for i := 1; i <= 2*N+1-2; i++ {
 		for j := 1; j <= 2*N+1-2; j++ {
 			p.OptimizeAt(i, j)
 		}
 	}
+
+	return p.S()
 }
 
-func (p *Poisson) S(i, j int, delta float64) float64 {
+func (p *Poisson) SLocal(i, j int, delta float64) float64 {
 	uij := p.U(i, j) + delta
 	a1 := p.U(i-1, j) + p.U(i+1, j) + p.U(i, j-1) + p.U(i, j+1)
 	a1 *= 2 * uij
@@ -73,4 +76,20 @@ func (p *Poisson) S(i, j int, delta float64) float64 {
 	a3 := p.Rho(i, j) * uij * math.Pow(p.dx, 2)
 
 	return -0.5*(a1-a2) - a3
+}
+
+func (p *Poisson) S() float64 {
+	var result float64
+	dx2 := math.Pow(p.dx, 2)
+	for i := 1; i <= 2*N+1-2; i++ {
+		for j := 1; j <= 2*N+1-2; j++ {
+			a1 := .5 * p.U(i, j) * (p.U(i+1, j) + p.U(i-1, j) - 2*p.U(i, j))
+			a1 /= dx2
+			a2 := .5 * p.U(i, j) * (p.U(i, j+1) + p.U(i, j-1) - 2*p.U(i, j))
+			a2 /= dx2
+			a3 := p.Rho(i, j) * p.U(i, j)
+			result += (a1 + a2 + a3) * dx2
+		}
+	}
+	return -result
 }
