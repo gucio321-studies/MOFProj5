99a92faf6ffccff7b9df3a8549783e05d65ea796
gucio321 <gucio321@protonmail.com>
2026-05-14 15:28:51 +0200

    add giu interface

--- go/main.go
+++ go/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/AllenDang/giu"
 	"github.com/gucio321-studies/MMFProj5/go/pkg"
 )
 
@@ -12,10 +11,24 @@ const (
 	d  = 4
 )
 
+func loop(ss []float64) {
+	giu.SingleWindow().Layout(
+		giu.Plot("Ex1").Plots(
+			giu.Line("SS", ss),
+		),
+	)
+}
+
 func main() {
 	p := pkg.NewPoisson(d, x0, dx, [3]float64{0, 0.5, 1})
-	for range 500 {
-		p.Optimize()
+	const nIter = 500
+	ss := make([]float64, nIter)
+	for i := range nIter {
+		ss[i] = p.Optimize()
 	}
-	fmt.Println(p)
+
+	wnd := giu.NewMasterWindow("Poisson", 640, 480, 0)
+	wnd.Run(func() {
+		loop(ss)
+	})
 }
